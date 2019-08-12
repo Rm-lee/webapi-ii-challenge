3082a90e220585e3b515b669216e32b6b3ce440a
Roger Lee <rmlee0003@gmail.com>
2019-08-11 20:37:06 -0400

    added endpoints for posting blog, posting comment, and getting posts

--- data/hubs-router.js
+++ data/hubs-router.js
@@ -1,7 +1,69 @@
-const express = require('express')
-const dbs = require('./db')
-const router = express.Router()
+const express = require("express");
+const dbs = require("./db");
+const router = express.Router();
 
+router.get('/', (req, res) => {
+ dbs.find()
+ .then(posts => {
+  res.status(200).json(posts)
+ })
+ .catch(error => {
+  res.status(500).json({
+   error: "The posts information could not be retrieved"
+  })
+ })
+});
 
+router.post("/", (req, res) => {
+  const newBlog = req.body;
+  if (newBlog.title && newBlog.contents) {
+    dbs
+      .insert(newBlog)
+      .then(id => {
+        dbs.findById(id.id).then(blog => {
+          res.status(201);
+          res.json(blog);
+        });
+      })
+      .catch(error => {
+        res.status(500);
+        res.json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  } else {
+    res.status(400);
+    res.json({
+      errorMessage: "Please provide title and contents for the post"
+    });
+  }
+});
 
-module.exports = router
+router.post("/:id/comments", (req, res) => {
+  const comment = req.body;
+  if (comment.text) {
+    dbs.insertComment(comment)
+     .then(id => {
+      dbs.findCommentById(id.id)
+        .then(postedComment => {
+          res.status(201).json(comment);
+        })
+        .catch(error => {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          });
+        });
+    })
+    .catch(error =>{
+     res.status(500).json({
+      error: "There was an error while saving the comment to the database"
+     })
+    })
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide text for the comment."
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const hubsRouter = require('./data/hubs-router')
-const dbs = require('./data/db')
+const hubsRouter = require('./data/hubs-router.js')
+const dbs = require('./data/db.js')
 
 const server = express();
 server.use(express.json());
-server.use(('/api/posts', hubsRouter))
+server.use('/api/posts', hubsRouter)
+
 
 module.exports = server
