02e4c266eaccfdbc9283ea0d43a02d7e5778240d
Roger Lee <rmlee0003@gmail.com>
2019-08-11 20:44:15 -0400

    added endpoint to get post by id using id sent in request params

--- data/hubs-router.js
+++ data/hubs-router.js
@@ -2,18 +2,40 @@ const express = require("express");
 const dbs = require("./db");
 const router = express.Router();
 
-router.get('/', (req, res) => {
- dbs.find()
- .then(posts => {
-  res.status(200).json(posts)
- })
- .catch(error => {
-  res.status(500).json({
-   error: "The posts information could not be retrieved"
-  })
- })
+router.get("/", (req, res) => {
+  dbs
+    .find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The posts information could not be retrieved"
+      });
+    });
 });
 
+router.get("/:id", (req,res) => {
+ const id = req.params.id
+ dbs.findById(id)
+ 
+ .then(post => {
+  if(post){
+  res.status(200).json(post)
+  }
+  else{
+   res.status(404).json({
+    message: "The post with the specified ID does not exist"
+   })
+  }
+})
+ .catch(error => {
+  res.status(500).json({
+   error: "The post information could not be retrieved."
+  })
+ })
+
+})
 router.post("/", (req, res) => {
   const newBlog = req.body;
   if (newBlog.title && newBlog.contents) {
